559af6520d8ce6a9444e96223b4b9b7b7f791315
tobstern <92857348+tobstern@users.noreply.github.com>
2021-12-07 00:06:38 +0100

    add some comments

--- day06.py
+++ day06.py
@@ -3,13 +3,16 @@ fishies = [int(i) for i in open('day06.txt', 'r').read().strip().split(',')]
 # Part 1:
 def populate(f, lim):
   i = 0
+  # use Counter (dictionary) for counting only occurences of each fish type (0-8).
   f = Counter(f)
   while i < lim:
     a = defaultdict(int)
     for l in f:
       if l > 0:
+        # decrease fish counter by 1
         a[l-1] += f[l]
       else:
+        # fish counter is 0 -> reset its counter to 6 and increase 'new fish' counter (8)
         a[6] += f[l]
         a[8] += f[l]
     f = a
